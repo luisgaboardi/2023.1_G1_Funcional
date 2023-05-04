f5b6bff61c578a87fb645f23d4060de5af6af05b
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2023-05-04 18:22:21 -0300

    Adiciona opção de escolha do usuário por diferentes temas de palavras para o jogo

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,19 @@
 module Main where
 
 import HangmanSay (hangmanSay)
-import Words (getPalavraAleatoria)
+import Words (getPalavraAleatoria, chooseFileFromFolder)
 import Match (novaPartida)
-
-
 
 main :: IO ()
 main = do
 
   hangmanSay "Hangman!"
-  
+
+  -- Chamada da função chooseFileFromFolder do módulo Words para obter o tema de palavras escolhido pelo usuário
+  categoria <- chooseFileFromFolder "./data"
+
   -- Chamada da função getPalavraAleatoria do módulo Words para obter uma palavra aleatória do arquivo palavras.txt
-  palavraSecreta <- getPalavraAleatoria "./data/palavras.txt"
+  palavraSecreta <- getPalavraAleatoria categoria
 
   -- Cria e imprime estado da partida
   let partida = novaPartida 6 palavraSecreta "" -- cria uma nova partida com 6 chances e a palavra secreta "PALAVRA"
--- src/Words.hs
+++ src/Words.hs
@@ -1,6 +1,44 @@
 module Words where
 
 import System.Random (randomRIO)
+import System.Directory (doesFileExist, getDirectoryContents)
+import System.FilePath ((</>), takeBaseName)
+import Control.Monad (filterM)
+
+getAllFilePaths :: FilePath -> IO [FilePath]
+getAllFilePaths path = do
+    content <- getDirectoryContents path
+    let filePathList = map (path </>) content
+    files <- filterM doesFileExist filePathList
+    return files
+
+getFileNames :: [FilePath] -> IO [(Int, String)]
+getFileNames filePaths = return $ zip [1..] (map takeBaseName filePaths)
+
+chooseFileFromFolder :: FilePath -> IO FilePath
+chooseFileFromFolder dir = do
+    filePaths <- getAllFilePaths dir
+    fileNames <- getFileNames filePaths
+    let numFiles = length filePaths
+    mapM_ (\(i, fileName) -> putStrLn (show i ++ ". " ++ fileName)) fileNames
+    putStrLn "Selecione o número do arquivo que você deseja: "
+    input <- getValidInput numFiles
+    let selectedFilePath = filePaths !! (input - 1)
+    return selectedFilePath
+
+getValidInput :: Int -> IO Int
+getValidInput maxInput = do
+    input <- getLine
+    case readMaybe input of
+        Just n | n >= 1 && n <= maxInput -> return n
+        _ -> do
+            putStrLn "Entrada inválida. Tente novamente."
+            getValidInput maxInput
+
+readMaybe :: Read a => String -> Maybe a
+readMaybe s = case reads s of
+    [(x,"")] -> Just x
+    _ -> Nothing
 
 getPalavraAleatoria :: FilePath -> IO String
 getPalavraAleatoria path = do
